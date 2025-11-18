a3540a4dbafda740f8a5d86e22ce1fb1dab918f3
Tausif Aman <tausifaman786@gmail.com>
2025-11-18 22:24:10 +0530

    Add files via upload

---
+++ Day13/script.js
@@ -0,0 +1,27 @@
+alert("Welcome")
+
+console.log("Code is running...")
+
+var a = prompt("Enter any number")
+var isTrue = confirm("Are you sure you want lo leave this page and blast your computer")
+if(isTrue){
+    console.log("Your computer is going to blast")
+}
+else{
+    console.log("Your computer is not going to blast")
+}
+
+console.log("Your number is "+a)
+
+document.title = "Introduction to NodeJs"
+
+let o = {
+    "name": "Tausif",
+    "job code": 4172
+}
+
+console.log(o)
+o.salary = "100crores";
+console.log(o)
+o.salary = "500crores";
+console.log(o)
