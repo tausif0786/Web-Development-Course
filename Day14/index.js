d8b0b6a667585e14984c2435a20d6a20636e6af5
Tausif Aman <tausifaman786@gmail.com>
2025-11-25 12:01:18 +0530

    Add files via upload

---
+++ Day14/index.js
@@ -0,0 +1,14 @@
+let arr = [1,2,4,5,7]
+
+console.log(arr)
+console.log(arr.length)
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+let a1 = [1,2,3]
+let a2 = [4,5,6]
+let a3 = [7,8,9]
+
+a1 = a1.concat(a2,a3)
+
+console.log(a1)
