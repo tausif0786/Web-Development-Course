8c27b1501e967f85d858f55affa9324284e20bc1
Tausif Aman <tausifaman786@gmail.com>
2025-11-30 19:36:17 +0530

    Add files via upload

---
+++ Day15/practice.js
@@ -0,0 +1,33 @@
+// let h2 = document.querySelector("h2");
+
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + " By Tausif Aman";
+
+// let divs = document.querySelectorAll(".box");
+
+// let index = 1;
+// for (div of divs){
+//     div.innerText = `new value is ${index}`;
+//     index++;
+// }
+
+// divs[0].innerText = "New Value is 1";
+// divs[1].innerText = "New value is 2";
+// divs[2].innerHTML = "New value is 3";
+
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click me!";
+
+// newBtn.style.color = "white";
+// newBtn.style.backgroundColor = "red";
+
+// document.querySelector("body").prepend(newBtn);
+
+console.log("Hello Tausif!")
---
+++ Day15/script.js
@@ -0,0 +1,5 @@
+// let heading = document.getElementsByClassName("myClass")
+
+// console.dir(heading)
+// console.log(heading)
+document.querySelector("div").children;
